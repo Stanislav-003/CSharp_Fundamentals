dac0a2b4a38dcb55dbccd8d6f739b90816157f02
Stanislav-003 <severin1975list@gmail.com>
2023-07-30 21:44:43 +0300

    CSharp_Fundamentals
    CSharp_Fundamentals

---
+++ HomeWork_2/HomeWork/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        //enum HTTPError 
+        //{
+        //    BadRequest = 400, 
+        //    Unauthorized, 
+        //    PaymentRequired, 
+        //    Forbidden, 
+        //    NotFound 
+        //};
+        //static void ChoseError()
+        //{
+        //    Console.WriteLine("Введіть номер помилки (400 - BadRequest, 401 - Unauthorized, 402 - PaymentRequired, 403 - Forbidden, 404 - NotFound): ");
+        //    HTTPError err = (HTTPError)int.Parse(Console.ReadLine());
+
+        //    string result = "";
+
+        //    switch (err)
+        //    {
+        //        case HTTPError.BadRequest:
+        //            result = "BadRequest";
+        //            break;
+        //        case HTTPError.Forbidden:
+        //            result = "Forbidden";
+        //            break;
+        //        case HTTPError.NotFound:
+        //            result = "NotFound";
+        //            break;
+        //        case HTTPError.PaymentRequired:
+        //            result = "PaymentRequired";
+        //            break;
+        //        case HTTPError.Unauthorized:
+        //            result = "Unauthorized";
+        //            break;
+        //    }
+        //    Console.Write("Ви ввели: {0}", result);
+        //}
+
+        //struct Dog
+        //{
+        //    public string name;
+        //    public string mark;
+        //    public int age;
+
+        //    public override string ToString()
+        //    {
+        //        return string.Format($"Собаку звати {name}, порода {mark}, вік {age} років.");
+        //    }
+        //}
+        
+
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("Введіть перше число: ");
+            //float a = float.Parse(Console.ReadLine());
+            //Console.Write((a >= -5) && (a <= 5) ? "Введіть друге число: " : "Число не підходить");
+            //float b = float.Parse(Console.ReadLine());
+            //Console.Write((b >= -5) && (b <= 5) ? "Введіть третє число: " : "Число не підходить");
+            //float с = float.Parse(Console.ReadLine());
+            //Console.Write((с >= -5) && (с <= 5) ? "Дякую!" : "Число не підходить");
+
+            //2
+            //Console.WriteLine("Введіть три числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine((a >= b) && (a >= c) ? $"Число {a} - max" :
+            //                  (b >= a) && (b >= c) ? $"Число {b} - max" :
+            //                  (c >= a) && (c >= b) ? $"Число {c} - max" : "Спробуйте ще раз!");
+
+            //Console.WriteLine((a <= b) && (a <= c) ? $"Число {a} - min" :
+            //                  (b <= a) && (b <= c) ? $"Число {b} - min" :
+            //                  (c <= a) && (c <= b) ? $"Число {c} - min" : "Спробуйте ще раз!");
+
+            //3
+            //ChoseError();
+
+            //4
+            //Dog myDog;
+
+            //Console.Write("Введіть назву собаки: ");
+            //myDog.name = Console.ReadLine();
+
+            //Console.Write("Введіть породу собаки: ");
+            //myDog.mark = Console.ReadLine();
+
+            //Console.Write("Введіть вік собаки: ");
+            //myDog.age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(myDog);
+        }
+    }
+}
