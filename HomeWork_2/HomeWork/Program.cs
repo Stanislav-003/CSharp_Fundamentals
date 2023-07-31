eca980008404c4a3f62cb607c355f7f943ecd72b
Stanislav-003 <severin1975list@gmail.com>
2023-07-31 20:36:48 +0300

    CSharpFundamentals
    CSharpFundamentals

--- HomeWork_2/HomeWork/Program.cs
+++ HomeWork_2/HomeWork/Program.cs
@@ -4,96 +4,93 @@ namespace HomeWork2
 {
     class Program
     {
-        //enum HTTPError 
-        //{
-        //    BadRequest = 400, 
-        //    Unauthorized, 
-        //    PaymentRequired, 
-        //    Forbidden, 
-        //    NotFound 
-        //};
-        //static void ChoseError()
-        //{
-        //    Console.WriteLine("Введіть номер помилки (400 - BadRequest, 401 - Unauthorized, 402 - PaymentRequired, 403 - Forbidden, 404 - NotFound): ");
-        //    HTTPError err = (HTTPError)int.Parse(Console.ReadLine());
+        enum HTTPError
+        {
+            BadRequest = 400,
+            Unauthorized,
+            PaymentRequired,
+            Forbidden,
+            NotFound
+        };
+        static void ChoseError()
+        {
+            Console.WriteLine("Введіть номер помилки (400 - BadRequest, 401 - Unauthorized, 402 - PaymentRequired, 403 - Forbidden, 404 - NotFound): ");
+            int err = Convert.ToInt32(Console.ReadLine());
 
-        //    string result = "";
+            if (Enum.IsDefined(typeof(HTTPError), err))
+            {
+                Console.WriteLine("Ви ввели: {0}", Enum.GetName(typeof(HTTPError), err));
+            }
+            else 
+            {
+                Console.WriteLine("Спробуйте ще раз!");
+            }
 
-        //    switch (err)
-        //    {
-        //        case HTTPError.BadRequest:
-        //            result = "BadRequest";
-        //            break;
-        //        case HTTPError.Forbidden:
-        //            result = "Forbidden";
-        //            break;
-        //        case HTTPError.NotFound:
-        //            result = "NotFound";
-        //            break;
-        //        case HTTPError.PaymentRequired:
-        //            result = "PaymentRequired";
-        //            break;
-        //        case HTTPError.Unauthorized:
-        //            result = "Unauthorized";
-        //            break;
-        //    }
-        //    Console.Write("Ви ввели: {0}", result);
-        //}
+        }
 
-        //struct Dog
-        //{
-        //    public string name;
-        //    public string mark;
-        //    public int age;
+        struct Dog
+        {
+            public string name;
+            public string mark;
+            public int age;
 
-        //    public override string ToString()
-        //    {
-        //        return string.Format($"Собаку звати {name}, порода {mark}, вік {age} років.");
-        //    }
-        //}
-        
+            public override string ToString()
+            {
+                return string.Format($"Собаку звати {name}, порода {mark}, вік {age} років.");
+            }
+        }
+
 
         static void Main(string[] args)
         {
             //1
-            //Console.Write("Введіть перше число: ");
-            //float a = float.Parse(Console.ReadLine());
-            //Console.Write((a >= -5) && (a <= 5) ? "Введіть друге число: " : "Число не підходить");
-            //float b = float.Parse(Console.ReadLine());
-            //Console.Write((b >= -5) && (b <= 5) ? "Введіть третє число: " : "Число не підходить");
-            //float с = float.Parse(Console.ReadLine());
-            //Console.Write((с >= -5) && (с <= 5) ? "Дякую!" : "Число не підходить");
+            Console.Write("Введіть перше число: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write((a >= -5) && (a <= 5) ? "Введіть друге число: " : "Число не підходить");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write((b >= -5) && (b <= 5) ? "Введіть третє число: " : "Число не підходить");
+            float с = float.Parse(Console.ReadLine());
+            Console.Write((с >= -5) && (с <= 5) ? "Дякую!" : "Число не підходить");
 
-            //2
-            //Console.WriteLine("Введіть три числа: ");
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //int b = Convert.ToInt32(Console.ReadLine());
-            //int c = Convert.ToInt32(Console.ReadLine());
+            //2.1
+            Console.WriteLine("Введіть три числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine((a >= b) && (a >= c) ? $"Число {a} - max" :
-            //                  (b >= a) && (b >= c) ? $"Число {b} - max" :
-            //                  (c >= a) && (c >= b) ? $"Число {c} - max" : "Спробуйте ще раз!");
+            Console.WriteLine((a >= b) && (a >= c) ? $"Число {a} - max" :
+                              (b >= a) && (b >= c) ? $"Число {b} - max" :
+                              (c >= a) && (c >= b) ? $"Число {c} - max" : "Спробуйте ще раз!");
 
-            //Console.WriteLine((a <= b) && (a <= c) ? $"Число {a} - min" :
-            //                  (b <= a) && (b <= c) ? $"Число {b} - min" :
-            //                  (c <= a) && (c <= b) ? $"Число {c} - min" : "Спробуйте ще раз!");
+            Console.WriteLine((a <= b) && (a <= c) ? $"Число {a} - min" :
+                              (b <= a) && (b <= c) ? $"Число {b} - min" :
+                              (c <= a) && (c <= b) ? $"Число {c} - min" : "Спробуйте ще раз!");
+
+            //2.2
+            Console.WriteLine("Введіть три числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("max: {0}", Math.Max(a, Math.Max(b, c)));
+            Console.WriteLine("min: {0}", Math.Min(a, Math.Min(b, c)));
 
             //3
-            //ChoseError();
+            ChoseError();
 
             //4
-            //Dog myDog;
+            Dog myDog;
 
-            //Console.Write("Введіть назву собаки: ");
-            //myDog.name = Console.ReadLine();
+            Console.Write("Введіть назву собаки: ");
+            myDog.name = Console.ReadLine();
 
-            //Console.Write("Введіть породу собаки: ");
-            //myDog.mark = Console.ReadLine();
+            Console.Write("Введіть породу собаки: ");
+            myDog.mark = Console.ReadLine();
 
-            //Console.Write("Введіть вік собаки: ");
-            //myDog.age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть вік собаки: ");
+            myDog.age = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine(myDog);
+            Console.WriteLine(myDog);
         }
     }
 }
--- HomeWork_2/Task/Program.cs
+++ HomeWork_2/Task/Program.cs
@@ -4,68 +4,68 @@ namespace Task2
 {
     class Program
     {
-        //enum TestCaseStatus { Pass, Fail, Blocked, WP, Unexecuted };
+        enum TestCaseStatus { Pass, Fail, Blocked, WP, Unexecuted };
 
-        //struct Color
-        //{
-        //    public byte R;
-        //    public byte G;
-        //    public byte B;
+        struct Color
+        {
+            public byte R;
+            public byte G;
+            public byte B;
 
-        //    public override string ToString()
-        //    {
-        //        return string.Format("{0}, {1}, {2}", R, G, B);
-        //    }
-        //}
+            public override string ToString()
+            {
+                return string.Format("{0}, {1}, {2}", R, G, B);
+            }
+        }
         static void Main(string[] args)
         {
             //1
-            //Console.Write("Введіть день: ");
-            //int dayOfMounth = Convert.ToInt32(Console.ReadLine());
-            //Console.Write((dayOfMounth >= 1) && (dayOfMounth <= 31) ? "Введіть місяць: " : "Такого дня не існує!");
-            //int mounthOfYear = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine((mounthOfYear >= 1) && (mounthOfYear <= 12) ? $"Сьогоднішня дата: {dayOfMounth}/{mounthOfYear}" : "Такого місяця не існує!");
-            //Console.WriteLine("");
+            Console.Write("Введіть день: ");
+            int dayOfMounth = Convert.ToInt32(Console.ReadLine());
+            Console.Write((dayOfMounth >= 1) && (dayOfMounth <= 31) ? "Введіть місяць: " : "Такого дня не існує!");
+            int mounthOfYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((mounthOfYear >= 1) && (mounthOfYear <= 12) ? $"Сьогоднішня дата: {dayOfMounth}/{mounthOfYear}" : "Такого місяця не існує!");
+            Console.WriteLine("");
 
 
             //2
-            //Console.Write("Введіть подвійне число: ");
-            //double a = Convert.ToDouble(Console.ReadLine());
-            //a %= 1;
-            //a *= 10;
-            //int b = (int)a;
-            //a %= 1;
-            //a *= 10;
-            //int c = (int)a;
-            //Console.WriteLine($"Сума: {b + c}");
-            //Console.WriteLine("");
+            Console.Write("Введіть подвійне число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            a %= 1;
+            a *= 10;
+            int b = (int)a;
+            a %= 1;
+            a *= 10;
+            int c = (int)a;
+            Console.WriteLine($"Сума: {b + c}");
+            Console.WriteLine("");
 
 
             //3
-            //Console.Write("Введіть годину: ");
-            //int hour = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine((hour >= 00) && (hour <= 05) ? "Доброї ночі!" :
-            //                  (hour >= 06) && (hour <= 12) ? "Доброго ранку!" :
-            //                  (hour >= 13) && (hour <= 17) ? "Добрий день!" :
-            //                  (hour >= 18) && (hour <= 23) ? "Добрий вечір!" : "Ви ввели неіснуючий час!");
-            //Console.WriteLine("");
+            Console.Write("Введіть годину: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((hour >= 00) && (hour <= 05) ? "Доброї ночі!" :
+                              (hour >= 06) && (hour <= 12) ? "Доброго ранку!" :
+                              (hour >= 13) && (hour <= 17) ? "Добрий день!" :
+                              (hour >= 18) && (hour <= 23) ? "Добрий вечір!" : "Ви ввели неіснуючий час!");
+            Console.WriteLine("");
 
 
             //4
-            //TestCaseStatus test1Status = TestCaseStatus.Pass;
-            //Console.WriteLine(test1Status);
+            TestCaseStatus test1Status = TestCaseStatus.Pass;
+            Console.WriteLine(test1Status);
 
 
             //5
-            //Color color;
-            //color.R = 255;
-            //color.G = 255;
-            //color.B = 255;
-            //Console.WriteLine("White " + color);
-            //color.R = 0;
-            //color.G = 0;
-            //color.B = 0;
-            //Console.WriteLine("Black " + color);
+            Color color;
+            color.R = 255;
+            color.G = 255;
+            color.B = 255;
+            Console.WriteLine("White " + color);
+            color.R = 0;
+            color.G = 0;
+            color.B = 0;
+            Console.WriteLine("Black " + color);
         }
     }
 }
