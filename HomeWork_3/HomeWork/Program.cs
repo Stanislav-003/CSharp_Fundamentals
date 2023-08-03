e5b1a2cd8f5b410642ceb95e32a21bdbc9af52d6
Stanislav-003 <severin1975list@gmail.com>
2023-08-03 03:07:03 +0300

    CSharp_Fundamentals
    CSharp_Fundamentals

---
+++ HomeWork_3/HomeWork/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.Write("Введіть будь-який рядок string: ");
+            string text = Console.ReadLine();
+
+            int a = 0;
+            int o = 0;
+            int i = 0;
+            int e = 0;
+            int n = 0;
+            while (n < text.Length)
+            {
+                switch (char.ToLower(text[n]))
+                {
+                    case 'a':
+                        a++;
+                        break;
+
+                    case 'o':
+                        o++;
+                        break;
+
+                    case 'i':
+                        i++;
+                        break;
+
+                    case 'e':
+                        e++;
+                        break;
+                }
+                n++;
+
+                //if (text[n] == 'a')
+                //{
+                //    a++;
+                //}
+                //if (text[n] == 'o')
+                //{
+                //    o++;
+                //}
+                //if (text[n] == 'i')
+                //{
+                //    i++;
+                //}
+                //if (text[n] == 'e')
+                //{
+                //    e++;
+                //}
+                //n++;
+            }
+            Console.WriteLine($"Кількість букв а = {a}");
+            Console.WriteLine($"Кількість букв o = {o}");
+            Console.WriteLine($"Кількість букв i = {i}");
+            Console.WriteLine($"Кількість букв e = {e}");
+
+            //2.1
+            Console.Write("Enter the month: ");
+            string mounth = Console.ReadLine();
+            Console.Write("Enter the year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            switch (mounth.ToLower())
+            {
+                case "january":
+                case "march":
+                case "may":
+                case "july":
+                case "august":
+                case "october":
+                case "december":
+                    Console.WriteLine("31");
+                    break;
+                case "april":
+                case "june":
+                case "september":
+                case "november":
+                    Console.WriteLine("30");
+                    break;
+                case "february":
+                    if (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))
+                    {
+                        Console.WriteLine("29");
+                    }
+                    else
+                    {
+                        Console.WriteLine("28");
+                    }
+
+                    //int daysInMonth = DateTime.DaysInMonth(year, 2);
+                    //Console.WriteLine(daysInMonth);
+                    break;
+            }
+
+            //2.2
+            Console.Write("Введіть рік: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть місяць: ");
+            int mounth = Convert.ToInt32(Console.ReadLine());
+
+            int daysInMonth = System.DateTime.DaysInMonth(year, mounth);
+            Console.WriteLine(daysInMonth);
+
+            //3
+            Console.WriteLine("Введіть 10 цілих чисел:");
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введіть елемент {i + 1}: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            bool sumOrMult = false;
+            int mult = 1;
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (array[i] < 0)
+                {
+                    sumOrMult = true;
+                    for (int k = 5; k < 10; k++)
+                    {
+                        mult *= array[k];
+                    }
+                }
+                sum += array[i];
+            }
+
+            if (sumOrMult == true)
+            {
+                Console.WriteLine(mult);
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ HomeWork_3/Task/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        enum name { water, pepsi, coffee }
+
+        static void choseName()
+        {
+            Console.WriteLine("Введіть назву напою (water, pepsi, coffee): ");
+            string drink = Console.ReadLine();
+
+            if (Enum.IsDefined(typeof(name), drink))
+            {
+                switch (drink.ToLower())
+                {
+                    case "water":
+                        Console.WriteLine("Ви обрали воду, ціна 10грн");
+                        break;
+                    case "pepsi":
+                        Console.WriteLine("Ви обрали пепсі, ціна 25грн");
+                        break;
+                    case "coffee":
+                        Console.WriteLine("Ви обрали каву, ціна 40грн");
+                        break;
+                    default:
+                        Console.WriteLine("Спробуйте ще раз!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Напій не знайдено!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //1
+            Console.Write("Введіть перше число: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть останнє число: ");
+            int lastNumber = Convert.ToInt32(Console.ReadLine());
+
+            int allIteration = 0;
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    allIteration++;
+                }
+            }
+            Console.WriteLine($"Всого чисел: {allIteration}");
+
+            //2
+            Console.WriteLine("Введіть будь-який текст: ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+                i = i + 1;
+            }
+
+            //3.1
+            Console.WriteLine("Введіть назву напою (Water, Pepsi, Coffee): ");
+            string name = Console.ReadLine();
+
+            switch (name.ToUpper())
+            {
+                case "WATER":
+                    Console.WriteLine("Ви обрали воду, ціна 10грн");
+                    break;
+                case "PEPSI":
+                    Console.WriteLine("Ви обрали пепсі, ціна 25грн");
+                    break;
+                case "COFFEE":
+                    Console.WriteLine("Ви обрали каву, ціна 40грн");
+                    break;
+                default:
+                    Console.WriteLine("Спробуйте ще раз!");
+                    break;
+            }
+
+            //3.2
+            choseName();
+
+            //4
+            Console.WriteLine("Введіть послідовність чисел: ");
+            double sum = 0;
+            double n = 0;
+            while (true)
+            {
+                double num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    break;
+                }
+                sum = sum + num;
+                n++;
+            }
+            Console.WriteLine($"Середнє арифметичне заданих чисел: {sum / n}");
+
+            //5
+            Console.Write("Введіть рік: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)))
+            {
+                Console.WriteLine($"{year} - високосний рік");
+            }
+            else
+            {
+                Console.WriteLine($"{year} - невисокосний рік");
+            }
+
+            //6
+            Console.Write("Введіть число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+
+            //7
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool Even = false;
+            while (number > 0)
+            {
+                if ((number % 10) % 2 != 0)
+                {
+                    Even = true;
+                    break;
+                }
+                number /= 10;
+            }
+
+            if (Even == true)
+            {
+                Console.WriteLine("Число містить непарні цифри");
+            }
+            else
+            {
+                Console.WriteLine("Число не містить непарні цифри");
+            }
+        }
+    }
+}
