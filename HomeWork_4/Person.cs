f52e96282f9f720ced5263c61a57943e132097e1
Stanislav-003 <severin1975list@gmail.com>
2023-08-07 13:05:02 +0300

    CSharp_Fundamentals
    CSharp_Fundamentals

---
+++ HomeWork_4/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_4
+{
+    class Person
+    {
+        private string name;
+        private int birthYear;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        public Person()
+        {
+            name = "Stanislav";
+            birthYear = 2003;
+        }
+
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - birthYear;
+            return age;
+        }
+
+        public static Person Input()
+        {
+            Console.Write("Введіть ім'я: ");
+            string name = Console.ReadLine();
+            Console.Write("Введіть рік народження: ");
+            int birthYear = int.Parse(Console.ReadLine());
+            Person person = new Person(name, birthYear);
+            return person;
+        }
+
+        public void ChangeName()
+        {
+            if (Age() < 16)
+            {
+                name = "Дуже молодий";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Ім'я: {name}, Рік народження: {birthYear}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.Name == person2.Name;
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return person1.Name != person2.Name;
+        }
+    }
+}
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Output();
+
+            Person person1 = new Person("Andriy", 2000);
+            person1.Output();
+
+            Person[] people = new Person[6];
+            people[0] = person;
+            people[1] = person1;
+
+            for (int i = 2; i < people.Length; i++)
+            {
+                people[i] = Person.Input();
+            }
+
+            Console.WriteLine("\nІнформація про людей:");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Output();
+                Console.WriteLine($"Вік: {people[i].Age()} років");
+            }
+
+            Console.WriteLine("\nЗміна імен для людей з віком менше 16:");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Age() < 16)
+                {
+                    people[i].ChangeName();
+                }
+            }
+
+            Console.WriteLine("\nОновлена інформація про людей:");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Output();
+            }
+
+            Console.WriteLine("\nЛюди з однаковими іменами:");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"Людина {i + 1} та людина {j + 1} мають однакове ім'я: {people[i].Name}");
+                    }
+                }
+            }
+        }
+    }
+}
